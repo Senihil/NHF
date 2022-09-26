b6255630c94408da5279a56cf25f020235f5f501
Senihil <takacsadri98@gmail.com>
2022-09-26 10:50:56 +0200

    Adding default to switch statement

--- NHF2/src/main/java/hu/nye/progtech/Main.java
+++ NHF2/src/main/java/hu/nye/progtech/Main.java
@@ -10,16 +10,22 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Kérem adja meg a nevét és a nyelvet (hu, en, ru)!\n");
         for (int i = 0; i < 2; i++) {
-            t[i] = myObj.next();
+            t[i] = myObj.nextLine();
         }
 
         switch (t[1]){
             case "hu":
                 System.out.printf("Szia, " + t[0] + "!");
+                break;
             case "en":
                 System.out.printf("Hi, " + t[0] + "!");
+                break;
             case "ru":
                 System.out.printf("Привет, " + t[0] + "!");
+                break;
+            default:
+                System.out.printf("Szia, " + t[0] + "!");
+                break;
         }
     }
 }
